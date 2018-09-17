99cbc2ac93ebbfaa350f1893c86a784dd9d94f0a
Ade Teriba <adekunleteriba@hotmail.com>
2018-09-17 16:40:27 +0100

    Update SimpleHTTPServer.java

--- SimpleHTTPServer.java
+++ SimpleHTTPServer.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class SimpleHTTPServer {
 	public static void main(String[] args) throws IOException {
 		
-		//I created a network socket which can accept connection on certain TCP port
+	    //I created a network socket which can accept connection on certain TCP port
 	    ServerSocket server = new ServerSocket(8082);
 	    System.out.println("Listening for connection on port 8082 ....");
 	    while (true){
@@ -25,13 +25,12 @@ public class SimpleHTTPServer {
 	    	 Once I ran the above the above code, 
 	    	 the server is now running listening for a connection, and reading HTTP requests.
 	    	 The server will just send today's date to the client using the following code
-	    	 
 	    	 */
 	    	try (Socket socket = server.accept()) {
                 Date today = new Date();
                 String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
                 socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
-                // Run he above program and connect using localhost:8082 on your browser and the date will be displayed
+                // Run the above program and connect using localhost:8082 on your browser and the date will be displayed
             }
 	  
 	    }
